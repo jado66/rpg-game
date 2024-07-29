c05c1c92cb481d2d1c7f9072c10fade8013558ac
Jadon Erwin <jadon.erwin@sinch.com>
2024-07-29 11:37:09 -0600

    Moved things around

---
+++ Assets/Scripts/UnhideUi.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnhideUi : MonoBehaviour
+{
+    // Reference to the Canvas component
+    public Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Unhide the Canvas by enabling it
+        if (canvas != null)
+        {
+            canvas.enabled = true;
+        }
+        else
+        {
+            Debug.LogWarning("Canvas component is not assigned.");
+        }
+    }
+}
